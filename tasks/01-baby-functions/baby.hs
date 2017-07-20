9e031ec2e81b8890554499e442fc7814cc3c32a8
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-07-20 23:55:22 +0200

    updating formatting

--- tasks/01-baby-functions/baby.hs
+++ tasks/01-baby-functions/baby.hs
@@ -5,8 +5,8 @@ doubleMe x = x + x
 doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100
-                    then x
-                    else x*2
+                      then x
+                      else x*2
 
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
