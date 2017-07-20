1f64189bb810d11c70eb7d48d5a9022a9880e6f0
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-07-21 00:05:21 +0200

    adding comments

--- tasks/01-baby-functions/baby.hs
+++ tasks/01-baby-functions/baby.hs
@@ -1,6 +1,10 @@
+-- first functions
+
 doubleMe x = x + x
 
 -- doubleUs x y = x*2 + y*2
+
+-- using one function inside another one
 
 doubleUs x y = doubleMe x + doubleMe y
 
@@ -8,6 +12,9 @@ doubleSmallNumber x = if x > 100
                       then x
                       else x*2
 
+-- using apostrophe syntax to mark function as strict version (not lazy)
+
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
 
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
