c1802dfce47f44bfd9af03eca24fa97fa8a9d5b8
Piotr Wittchen <piotr.wittchen@sap.com>
2017-07-21 12:31:33 +0200

    adding 02_lists.hs file

---
+++ tasks/02_lists.hs
@@ -0,0 +1,21 @@
+-- lists
+
+-- creating lists
+
+firstList = [1,2,3]
+secondList = [4,5,6]
+
+-- joining lists
+
+thirdList = firstList ++ secondList
+helloworld = "hello " ++ "world"
+
+-- appending item in the beginning of the list
+
+asmallcat = 'A':"SMALL CAT"
+somethingAtStart = 5:[1,2,3]
+
+-- accessing index of the list
+
+letter = "Steve Buscemi" !! 6
+value = [9.4,33.2,96.2,11.2,23.25] !! 1
