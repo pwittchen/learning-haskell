50d72528a8c7c0843605e52ea47723e68d767e5e
Piotr Wittchen <piotr.wittchen@sap.com>
2017-11-14 16:16:36 +0100

    adding more samples with types

--- tasks/03_types_and_typeclasses.hs
+++ tasks/03_types_and_typeclasses.hs
@@ -1,3 +1,16 @@
 -- types and typeclasses
+
 removeNonUpperCase st = [c | c <- st, c `elem` ['A'..'Z']]
 -- now, we can call removeNonUpperCase "AaBb"
+
+addThree x y z = x + y + z
+
+factorial n = product[1..n]
+
+circumference r = 2 * pi * r
+
+-- :t head
+-- head is generic tipe (takes list of any type and returns element of that type)
+
+-- typeclasses
+-- :t (==)
