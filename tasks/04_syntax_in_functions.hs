099a9ccdc6e416f69352bee1d3b70ba47cc8f2f6
Piotr Wittchen <piotr.wittchen@sap.com>
2017-11-16 17:46:56 +0100

    adding sample with factorial

--- tasks/04_syntax_in_functions.hs
+++ tasks/04_syntax_in_functions.hs
@@ -3,3 +3,7 @@
 lucky :: (Integral a) => a -> String
 lucky 7 = "Lucky number 7!"
 lucky x = "sorry, you missed it"
+
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial(n - 1)
