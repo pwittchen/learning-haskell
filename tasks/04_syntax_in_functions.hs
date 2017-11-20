7090183e37257716cffea188bd1a97fb867fdbe9
Piotr Wittchen <piotr.wittchen@sap.com>
2017-11-20 16:02:13 +0100

    adding more samples with syntax and functions

--- tasks/04_syntax_in_functions.hs
+++ tasks/04_syntax_in_functions.hs
@@ -7,3 +7,29 @@ lucky x = "sorry, you missed it"
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = n * factorial(n - 1)
+
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+first :: (a, b, c) -> a
+first (x, _, _) = x
+
+second :: (a, b, c) -> b
+second (_, y, _) = y
+
+third :: (a, b, c) -> c
+third (_, _, z) = z
+
+head' :: [a] -> a
+head' [] = error "Can't call head on an empty list, dummy!"
+head' (x:_) = x
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y 
+
+length' :: (Num b) => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs  
